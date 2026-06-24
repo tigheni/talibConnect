4cb75c0838855325dc5e23c7a6041699239b49c0
tigheni <oussama.adame12@gmail.com>
2026-06-24 19:31:15 +0100

    refector

---
+++ src/pages/ExamViewer.jsx
@@ -0,0 +1,11 @@
+import { IoIosArrowBack } from "react-icons/io";
+export default function ExamViewer() {
+  return (
+    <div className="">
+      <button className="text-[50px] text-[#4fd86f] rounded-lg bg-gray-300/60 m-4 hover:bg-gray-400/40 hover:scale-[1.05] transition-all duration-200">
+        <IoIosArrowBack />
+      </button>
+      <h1>g</h1>
+    </div>
+  );
+}
