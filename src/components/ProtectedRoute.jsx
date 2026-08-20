beeb2e0fca7292b782f3b950425a181168e2d177
tigheni <oussama.adame12@gmail.com>
2026-08-20 03:54:01 +0100

    refactor: simplify ProtectedRoute component by using useAuth for user state management

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,30 +1,23 @@
-import { useNavigate, useLocation } from "react-router-dom";
-import { getUser } from "../services/getUser";
+import { Navigate, useLocation } from "react-router-dom";
 import LoadingSpinner from "./LoadingSpinner";
-import { useEffect, useState } from "react";
+import { useAuth } from "../context/AuthContext";
 
 export default function ProtectedRoute({ children }) {
-  const [loading, setLoading] = useState(true);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const navigate = useNavigate();
+  const { user, loading } = useAuth();
   const location = useLocation();
-  useEffect(() => {
-    const checkUser = async () => {
-      setLoading(true);
-
-      const { user } = await getUser();
-      if (!user) {
-        navigate(`/login?redirect=${location.pathname}`, { replace: true });
-      } else {
-        setIsAuthenticated(true);
-      }
-      setLoading(false);
-    };
-    checkUser();
-  }, [navigate, location.pathname]);
 
   if (loading) {
     return <LoadingSpinner />;
   }
-  return isAuthenticated ? children : null;
+
+  if (!user) {
+    return (
+      <Navigate
+        to={`/login?redirect=${encodeURIComponent(location.pathname)}`}
+        replace
+      />
+    );
+  }
+
+  return children;
 }
